6d03ed1dd34987b64d5f6b7635f9b8d9c705cb5f
Akihisa Yamaguchi <71268501+albasiempre@users.noreply.github.com>
2024-03-31 14:40:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.listen(PORT, () => console.log("サーバーが起動しました"));
 // Corsの設定
 
 app.use(cors({
-  origin: 'https://reactjs-realtime-chat.vercel.app/', //アクセス許可するオリジン
+  origin: 'https://reactjs-realtime-chat.vercel.app', //アクセス許可するオリジン
   credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
   optionsSuccessStatus: 200 //レスポンスstatusを200に設定
 }))
