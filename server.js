3f6247dda2092872d30a9ba26012e9938bc1f4c6
Akihisa Yamaguchi <71268501+albasiempre@users.noreply.github.com>
2024-04-01 21:51:08 +0900

    change a bit on cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const conversationRoute = require("./routes/conversations");
 const messageRoute = require("./routes/messages");
 // const MyPageRoute = require("./routes/mypage");
 
-app.use(function(res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
