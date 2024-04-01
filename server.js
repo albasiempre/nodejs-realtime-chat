bafc752374b9df994730b6f4619ea5a663f955b4
Akihisa Yamaguchi <71268501+albasiempre@users.noreply.github.com>
2024-04-01 19:57:57 +0900

    サブドメインでのcors回避

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.listen(PORT, () => console.log("サーバーが起動しました"));
 // Corsの設定
 
 app.use(cors({
-  origin: 'https://reactjs-realtime-chat.vercel.app', //アクセス許可するオリジン
+  origin: 'https://client.albasiempre.com', //アクセス許可するオリジン
   credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
   optionsSuccessStatus: 200 //レスポンスstatusを200に設定
 }))
